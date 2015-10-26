5c7f9bb5971e7acf52f6139a8beaf5c60f707a5f
Alexa <amzeazas@comcast.net>
2015-10-26 16:45:47 -0700

    Add new user sign up confirmation functionality.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,12 +1,12 @@
 class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable and :omniauthable
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable
+  # :lockable, :timeoutable and :omniauthable
+  devise :database_authenticatable, :registerable, :confirmable, :recoverable, :rememberable, :trackable, :validatable
 
   validates :email, :presence => true
   validates :username, :presence => true
   validates :username, :uniqueness => true
+  validates :username, format: { with: /\A[a-zA-Z0-9_\.]*\z/ }
   has_attached_file :avatar, styles: { medium: "300x300>", thumb: "100x100>" }, default_url: "/images/:style/missing.png"
   validates_attachment_content_type :avatar, content_type: /\Aimage\/.*\Z/
 end
